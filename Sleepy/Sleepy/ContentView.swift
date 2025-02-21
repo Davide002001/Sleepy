559dda911e5c8b721221467f8e6010f6622f44b3
Davide Perrotta <74309910+Davide002001@users.noreply.github.com>
2025-02-21 19:22:20 +0100

    Add files via upload

---
+++ Sleepy/Sleepy/ContentView.swift
@@ -0,0 +1,155 @@
+//
+//  ContentView.swift
+//  Sleepy
+//
+//  Created by Davide Perrotta on 19/02/25.
+//
+
+import SwiftUI
+import HealthKit
+
+struct ContentView: View {
+    @State private var sleepData: [HKCategorySample] = []
+    @StateObject private var health = HealthManager()
+    
+    @State private var showWelcomeSheet = UserDefaults.isFirstLaunch
+    
+    
+    @State private var apiResponse: String = ""
+    @State private var sleepTime: Double = 0
+    @State private var isLoading: Bool = false
+    
+    @AppStorage("timeInBed") private var savedTimeInBed: Int = 0
+    
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor.systemGray6
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    
+    }
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 20) {
+                    
+                    // 🛌 Media Secondi a Letto
+                    HStack(spacing: 5) {
+                        Text("🛌")
+                            .font(.system(size: 17))
+                        Text("MEDIA SEC. A LETTO")
+                            .font(.system(size: 16, weight: .regular))
+                            .bold()
+                            .foregroundColor(.gray)
+                            .padding(.top, 5)
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                    .padding(.top, 20)
+
+                    // Valore tempo a letto
+                    HStack {
+                        Text("\(savedTimeInBed)")
+                            .font(.system(size: 45, weight: .regular))
+                        Text("secondi")
+                            .foregroundColor(.gray)
+                            .font(.system(size: 15, weight: .regular))
+                            .padding(.top, 20)
+                        Spacer()
+                    }
+                    .padding(.leading)
+                    
+                    HStack {
+                        Text("Per visualizzare i dati, alla prima apertura, clicca \n su \"Aggiungi Dati\" in alto a destra e poi premi \"Salva\".")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .multilineTextAlignment(.center)
+                    }
+                    .padding()
+                    .onAppear{
+                        UserDefaults.standard.set(true, forKey: "hasLaunchedBefore")
+                    }
+
+                    // Lista Sonno
+                    SleepTrackerTabView()
+                        .frame(height: 280)
+                        .padding(.top, -45)
+                    
+                    SleepButtonsView()
+                    if isLoading {
+                        ProgressView()
+                            .padding()
+                    }
+                    
+                    if !apiResponse.isEmpty {
+                        Text(apiResponse)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.gray.opacity(0.3))
+                            .cornerRadius(30)
+                            .padding(.horizontal, 24)
+                    }
+                    SleepInfoView()
+                    SleepMoreInfoView()
+                }
+                .padding(.top, 20)
+            }
+            .background(Color.black.edgesIgnoringSafeArea(.all))
+            .navigationTitle("Sonno")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink("Aggiungi Dati", destination: AddDataView(health: health, apiResponse: $apiResponse, isLoading: $isLoading))
+                        .font(.system(size: 18, weight: .regular))
+                }
+            }
+        }
+        .onAppear {
+            health.requestSleepAuthorization()
+            UserDefaults.standard.set(true, forKey: "hasLaunchedBefore")
+            resetTimeInBedIfNeeded()
+               
+            if UserDefaults.standard.object(forKey: "timeInBed") == nil {
+                savedTimeInBed = Int(health.timeInBed)  // Imposta il valore iniziale da HealthKit
+            }
+        }
+        .sheet(isPresented: $showWelcomeSheet) {
+            WelcomeView {
+                showWelcomeSheet = false
+            }
+            .background(Color(UIColor.systemBackground))
+        }
+    }
+    
+    func resetTimeInBedIfNeeded() {
+        let lastResetDate = UserDefaults.standard.object(forKey: "lastResetDate") as? Date ?? Date.distantPast
+        let calendar = Calendar.current
+        
+        if !calendar.isDateInToday(lastResetDate) {
+            savedTimeInBed = 0
+            UserDefaults.standard.set(Date(), forKey: "lastResetDate") // Aggiorna la data dell'ultimo reset
+        }
+    }
+}
+
+extension UserDefaults {
+    static var isFirstLaunch: Bool {
+        get {
+            let hasLaunched = standard.bool(forKey: "hasLaunchedBefore")
+            if !hasLaunched {
+                standard.set(true, forKey: "hasLaunchedBefore")
+            }
+            return !hasLaunched
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
