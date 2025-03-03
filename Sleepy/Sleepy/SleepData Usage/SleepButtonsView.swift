2a795b62eea0a5aa6b82b63c70552c8c49c9e061
Davide Perrotta <74309910+Davide002001@users.noreply.github.com>
2025-03-03 19:52:31 +0100

    Add files via upload

---
+++ Sleepy/Sleepy/SleepData Usage/SleepButtonsView.swift
@@ -0,0 +1,155 @@
+//
+//  SleepButtonsView.swift
+//  Sleepy
+//
+//  Created by Davide Perrotta on 20/02/25.
+//
+
+import SwiftUI
+
+struct SleepButtonsView: View {
+    
+    @State private var showAlert = false
+    @State private var alertMessage = ""
+    @State private var alertTitle = ""
+    @State private var isLoading: Bool = false
+    @State private var apiResponse: String = ""
+    
+    @StateObject private var health = HealthManager()
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            Button(action: {
+                Task {
+                    if health.timeInBed > 0 { // Evita di inviare 0 al server, perchè al primo avvio potrebbe inviare 0 
+                        await sendSleepTime()
+                    } else {
+                        apiResponse = "⚠️ Dati non disponibili, riprova dopo la sincronizzazione."
+                        alertTitle = "⚠️ Attenzione"
+                        alertMessage = "I dati del sonno non sono ancora stati caricati."
+                        showAlert = true
+                    }
+                    health.fetchSleepdata() // Aggiorna i dati dopo l'invio
+                }
+            }) {
+                HStack {
+                    Image(systemName: "clock")
+                    Text("Sincronizza sonno con il server")
+                        .fontWeight(.semibold)
+                }
+                .foregroundColor(.white)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .cornerRadius(12)
+            }
+            
+            Button(action: {
+                Task {
+                    await sendSleepDetails()
+                    health.fetchSleepdata()
+                }
+            }) {
+                HStack {
+                    Image(systemName: "square.and.pencil")
+                    Text("Aggiorna fasi sonno con il server")
+                        .fontWeight(.semibold)
+                }
+                .foregroundColor(Color.green)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color(UIColor.systemGreen).opacity(0.2))
+                .cornerRadius(12)
+            }
+            .padding(.top, 10)
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+            }
+
+            if isLoading {
+                ProgressView()
+                    .padding()
+            }
+            
+            if !apiResponse.isEmpty {
+                Text(apiResponse)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.gray.opacity(0.3))
+                    .cornerRadius(30)
+                    .padding(.horizontal, 24)
+            }
+            
+        }
+        .padding()
+        .onAppear {
+            Task {
+                health.fetchSleepdata() // Carica i dati subito quando la view appare
+            }
+        }
+    }
+    
+    // Funzioni per API
+    
+    private func sendSleepTime() async {
+        isLoading = true
+        do {
+            let response = try await NetworkManager.shared.sendPost_request(seconds: Int(health.timeInBed))
+            await MainActor.run{
+                apiResponse = "✅ Successo : \(response)"
+                alertTitle = "✅ Successo"
+                alertMessage = "\n Dati inviati correttamente"
+                showAlert = true
+                isLoading = false
+            }
+        } catch {
+            await MainActor.run{
+                apiResponse = "❌ Successo : \(error.localizedDescription)"
+                alertTitle = "❌ Errore. \n Riprova più tardi"
+                alertMessage = "\n Riprova più tardi"
+                showAlert = true
+                isLoading = false
+            }
+            
+            apiResponse = "❌ Errore: \(error.localizedDescription)"
+        }
+        isLoading = false
+    }
+    
+    private func sendSleepDetails() async {
+        await MainActor.run {isLoading = true}
+            do {
+                let sleepDetails = await health.fetchSleepDataForAPI()
+                let response = try await NetworkManager.shared.sendSleepDetails(sleepData: sleepDetails)
+               
+                await MainActor.run{
+                    apiResponse = "✅ Successo : \(response)"
+                    alertTitle = "✅ Successo"
+                    alertMessage = "\n Dati inviati correttamente"
+                    showAlert = true
+                    isLoading = false
+                }
+                
+            } catch {
+                print("❌ Errore nell'invio dei dettagli del sonno: \(error.localizedDescription)") // Per il Debug
+                
+                await MainActor.run{
+                    apiResponse = "❌ Successo : \(error.localizedDescription)"
+                    alertTitle = "❌ Errore. \n Riprova più tardi"
+                    alertMessage = "Riprova più tardi"
+                    showAlert = true
+                    isLoading = false
+                }
+            }
+    }
+    
+
+}
+
+
+struct SleepButtonsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SleepButtonsView()
+            .preferredColorScheme(.dark) // Forza il tema scuro
+    }
+}
---
+++ Sleepy/Sleepy/SleepData Usage/SleepSummaryView.swift
@@ -0,0 +1,86 @@
+//
+//  SleepSummaryView.swift
+//  Sleepy
+//
+//  Created by Davide Perrotta on 24/02/25.
+//
+
+import SwiftUI
+
+struct SleepSummaryView: View {
+    
+    @ObservedObject var health : HealthManager
+    
+    @State private var showSheet = false  // Stato per mostrare il foglio delle info sulle fasi del sonno
+    
+    var hours: Int {
+        Int(health.timeInBed) / 3600
+    }
+        
+    var minutes: Int {
+        (Int(health.timeInBed) % 3600) / 60
+    }
+        
+    var seconds: Int {
+        Int(health.timeInBed) % 60
+    }
+    
+    
+    var body: some View {
+        HStack(spacing: 5) {
+            Text("🛌")
+                .font(.system(size: 17))
+            Text("MEDIA ORE A LETTO")
+                .font(.system(size: 16, weight: .regular))
+                .bold()
+                .foregroundColor(.gray)
+                .padding(.top, 5)
+                
+            Spacer()
+            
+            HStack {
+                Image(systemName: "info.circle")
+                    .foregroundColor(.blue)
+                    .font(.system(size: 24))
+                    .onTapGesture {
+                        showSheet = true  // Mostra il foglio
+                    }
+            }
+            .sheet(isPresented: $showSheet) {
+                Sleephasesinfo() // Mostra la tua View
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.leading)
+        .padding(.top, 20)
+        
+        HStack {
+            HStack(alignment: .lastTextBaseline) {
+                Text("\(hours)")
+                    .font(.system(size: 45, weight: .bold))
+                    .foregroundColor(.white)
+                
+                Text("h")
+                    .font(.system(size: 24, weight: .medium))
+                    .foregroundColor(.gray)
+                
+                Text("\(minutes)")
+                    .font(.system(size: 35, weight: .bold))
+                    .foregroundColor(.white)
+                
+                Text("min")
+                    .font(.system(size: 24, weight: .medium))
+                    .foregroundColor(.gray)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading, 20)
+            .onAppear{
+                health.fetchSleepdata()
+            }
+        }
+    }
+}
+
+#Preview {
+    SleepSummaryView(health: HealthManager())
+}
