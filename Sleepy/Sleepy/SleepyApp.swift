1fe29e922dc67819d01ef550ff04a567f38bbc60
Davide Perrotta <74309910+Davide002001@users.noreply.github.com>
2025-03-05 00:58:31 +0100

    Add files via upload

---
+++ Sleepy/Sleepy/SleepyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepyApp.swift
+//  Sleepy
+//
+//  Created by Davide Perrotta on 20/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SleepyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
